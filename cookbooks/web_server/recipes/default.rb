e79edc7982b5bca753d05e14e731087f823437a3
student <student@localhost.localdomain>
2018-05-11 14:42:49 +0300

    ke2

--- cookbooks/web_server/recipes/default.rb
+++ cookbooks/web_server/recipes/default.rb
@@ -1,12 +1,9 @@
-# Cookbook Name:: web
-# Recipe:: default
-
 web 'jboss or tomcat' do 
   action [:install, :setup, :restart]
   if node.role?('web_jboss')
-    provider 'web_jboss'
+    cookbook 'web_jboss'
   elsif node.role?('web_tomcat')
-    provider 'web_tomcat'
+    cookbook 'web_tomcat'
   else
   	puts "please try jboss or tomcat role"
   end
